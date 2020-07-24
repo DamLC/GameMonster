2f5ea16efb79aa25334fa98cc801138f36d4e0a6
damien.loconte <dlc65@free.fr>
2020-07-24 18:47:29 +0200

    add class

---
+++ GameMonster/Classes/Archer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Permissions;
+using TPjeu.Accessoire;
+using TPjeu.Personnages;
+
+namespace TPjeu.Classe
+{
+    public class Archer : Joueur
+    {
+        public static int degats;
+
+        public Archer(int pointVie, int plastron, int epee, int rouge) : base(pointVie, plastron, epee, rouge)
+        {
+
+        }
+
+        public static int skill()
+        {
+            int recharge = De.lancerDe(10);
+            int competence = De.lancerDe(6);
+            
+            if (recharge > 5)
+            {
+                Narration.recharge();
+                Console.WriteLine("Vous avez fait " + competence);
+
+                if (competence >= 4)
+                {
+                    Narration.competenceArcher();
+                    degats = De.lancerDe(26) + 100;
+                }
+                else
+                {
+                    Console.WriteLine("Ah c'est dommage la compêtence a échoué");
+                    degats = De.lancerDe(26) + arme.degatsCAC;
+                }
+            }
+
+            return degats;
+        }
+        
+       
+
+        
+    }
+}
---
+++ GameMonster/Classes/Mage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TPjeu.Accessoire;
+using TPjeu.Personnages;
+
+namespace TPjeu.Classe
+{
+    public class Mage : Joueur
+    {
+        public static int degats;
+        public Mage(int pointVie, int plastron, int epee, int rouge) : base(pointVie, plastron, epee, rouge)
+        {
+        }
+
+        public static int skill()
+        {
+            int recharge = De.lancerDe(10);
+            int competence = De.lancerDe(6);
+
+
+            if (recharge == 5)
+            {
+                Narration.recharge();
+                Console.WriteLine("Vous avez fait " + competence);
+
+                if (competence == 4)
+                {
+                    Narration.competenceMage();
+                    degats = De.lancerDe(26) + 100;
+                }
+                else
+                {
+                    Console.WriteLine("Ah c'est dommage la compêtence a échoué");
+                    degats = De.lancerDe(26) + arme.degatsCAC;
+                }
+            }
+
+            return degats;
+        }
+    }
+}
