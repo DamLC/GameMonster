53877ba145555930cff1791ab2eb9ce5e46aa50b
damien.loconte <dlc65@free.fr>
2020-07-24 18:48:41 +0200

    add choice class of player

--- GameMonster/Niveaux/LevelBoss.cs
+++ GameMonster/Niveaux/LevelBoss.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using TPjeu.Armes;
+using TPjeu.Classe;
 using TPjeu.Personnages;
 using TPjeu.Protections;
 
@@ -7,11 +8,59 @@ namespace TPjeu
 {
     public class LevelBoss
     {
+        public static Joueur hero ;
+        
         public static void lastStage()
-        {
+        {  
+            string classe;
+            
+                     
+                     
             Narration.boss();
-            Narration.creationJoueur();
-            Joueur hero = new Joueur(CreationPersonnages.creePV(),CreationPersonnages.creePA(), CreationPersonnages.creeDegats(), 50 );
+          
+            Console.WriteLine("Choisissez votre classe :\n" +
+                              "1 > Guerrier\n" +
+                              "2 > Mage\n" +
+                              "3 > Archer\n" +
+                              "Attention une réponse de 0 ou plus grand que 3 est à vos risques et périls");
+            classe = Console.ReadLine();
+            
+            switch (classe)
+            {
+                case "1":
+                {
+                    Console.WriteLine("Vous avez choisi guerriers votre arme sera donc l'épée\n");
+                    Narration.creationGuerrier();
+                    hero = new Guerrier(CreationPersonnages.creePV(),CreationPersonnages.creePA(),CreationPersonnages.creeDegats(),CreationPersonnages.potionPv());
+                    Narration.nom();
+                    break;
+                }
+                case "2":
+                {
+                    Console.WriteLine("Vous avez choisi mages votre arme sera donc le baton\n");
+                    Narration.creationMage();
+                    hero = new Mage(CreationPersonnages.creePV(),CreationPersonnages.creePA(),CreationPersonnages.creeDegats(),CreationPersonnages.potionPv());
+                    Narration.nom();
+                    break;
+                }
+                case "3":
+                {
+                    Console.WriteLine("Vous avez choisi archers votre arme sera donc l'arc\n");
+                    Narration.creationArcher();
+                    hero = new Archer(CreationPersonnages.creePV(),CreationPersonnages.creePA(),CreationPersonnages.creeDegats(),CreationPersonnages.potionPv());
+                    Narration.nom();
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Vous avez choisi un hero banal sans compêtence particulière\n");
+                    Narration.creationJoueur();
+                    hero = new Joueur(CreationPersonnages.creePVLambda(),CreationPersonnages.creePALambda(), CreationPersonnages.creeDegatsLambda());
+                    Narration.nom();
+                    break;
+                }
+            }      
+            Narration.enter();
             Narration.creationBoss();
             Boss gontrand = new Boss(CreationPersonnages.creePV(), CreationPersonnages.creeDegats());
          
